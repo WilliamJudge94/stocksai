6d7f6078ae9cd763081b2a9205348a1c96de75a0
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2024-01-28 15:07:33 -0600

    FEAT: auto-train

--- src/stocksai/ai/utils.py
+++ src/stocksai/ai/utils.py
@@ -29,3 +29,20 @@ def get_batch(data, batch_size, block_size, device, noise_stdev=5):
     # y = (y.float() + noise).round().long()
 
     return x.to(device), y.to(device)
+
+
+def append_to_tensor(original_tensor, values_to_append):
+    # Convert values_to_append to a tensor if it is not already
+    if not isinstance(values_to_append, torch.Tensor):
+        values_to_append = torch.tensor(values_to_append,
+                                        dtype=original_tensor.dtype)
+
+    # Create a new tensor with the size of the original tensor plus the size of values_to_append
+    new_tensor_size = original_tensor.nelement() + values_to_append.nelement()
+    new_tensor = torch.empty(new_tensor_size, dtype=original_tensor.dtype)
+
+    # Fill the new tensor with the original values and the new values
+    new_tensor[:original_tensor.nelement()] = original_tensor
+    new_tensor[original_tensor.nelement():] = values_to_append
+
+    return new_tensor
