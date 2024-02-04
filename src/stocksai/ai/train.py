9f48c9ef20f76db7be1685384f55ed2eed6f1e38
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2024-02-04 12:42:32 -0600

    BUG: removing ai from repo

--- src/stocksai/ai/train.py
+++ src/stocksai/ai/train.py
@@ -1,97 +1,97 @@
-from stocksai.ta import calculate_indicators
-from stocksai.ai.model import TransformerModel
-from stocksai.ai.utils import get_batch
-from torch.nn import functional as F
-import plotly.express as px
-import torch.optim as optim
-from tqdm import tqdm
-import torch.nn as nn
-import numpy as np
-import torch
+# from stocksai.ta import calculate_indicators
+# from stocksai.ai.model import TransformerModel
+# from stocksai.ai.utils import get_batch
+# from torch.nn import functional as F
+# import plotly.express as px
+# import torch.optim as optim
+# from tqdm import tqdm
+# import torch.nn as nn
+# import numpy as np
+# import torch
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
+# device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-hist = calculate_indicators(period='10y',
-                            interval='1d')
-
-
-# take an input sequence
-data = hist['Close'].to_numpy()  
-data = data * 10
-
-data = np.expand_dims(data, axis=0)
-data = torch.tensor(data)
-
-training_data = data[:, :2000]
-testing_data = data[:, 2000:]
+# hist = calculate_indicators(period='10y',
+#                             interval='1d')
 
 
-# Example hyperparameters
-batch_size = 10
-vocab_size = len(np.unique(data.numpy().flatten())) * 10
-d_model = 10
-nhead = 1
-num_encoder_layers = 1
-dim_feedforward = 1
-max_seq_length = 30
+# # take an input sequence
+# data = hist['Close'].to_numpy()
+# data = data * 10
 
-# Create and move the model to the appropriate device
-model = TransformerModel(vocab_size,
-                         d_model,
-                         nhead,
-                         num_encoder_layers,
-                         dim_feedforward,
-                         max_seq_length,
-                         device=device).to(device)
+# data = np.expand_dims(data, axis=0)
+# data = torch.tensor(data)
 
-# Generate a batch of data
-x, y = get_batch(training_data, batch_size, max_seq_length, device)
-
-# # Forward pass
-output = model(x.long().to(device))
-# print(output.shape)
+# training_data = data[:, :2000]
+# testing_data = data[:, 2000:]
 
 
-# Define Loss Function and Optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.AdamW(model.parameters(), lr=3e-4)
+# # Example hyperparameters
+# batch_size = 10
+# vocab_size = len(np.unique(data.numpy().flatten())) * 10
+# d_model = 10
+# nhead = 1
+# num_encoder_layers = 1
+# dim_feedforward = 1
+# max_seq_length = 30
 
-training_data = training_data.to(device)
+# # Create and move the model to the appropriate device
+# model = TransformerModel(vocab_size,
+#                          d_model,
+#                          nhead,
+#                          num_encoder_layers,
+#                          dim_feedforward,
+#                          max_seq_length,
+#                          device=device).to(device)
 
-# Number of epochs for training
-num_epochs = 40
+# # Generate a batch of data
+# x, y = get_batch(training_data, batch_size, max_seq_length, device)
 
-for epoch in range(num_epochs):
-    model.train()  # Set the model to training mode
-    total_loss = 0
+# # # Forward pass
+# output = model(x.long().to(device))
+# # print(output.shape)
 
-    # Iterate over the training data in batches
-    for _ in tqdm(range(training_data.shape[1] // batch_size), desc="Batch"):
-        # Generate a batch of data
-        src, tgt = get_batch(training_data, batch_size, max_seq_length, device)
 
-        optimizer.zero_grad()  # Clear the gradients from the previous iteration
+# # Define Loss Function and Optimizer
+# criterion = nn.CrossEntropyLoss()
+# optimizer = optim.AdamW(model.parameters(), lr=3e-4)
 
-        # Forward pass: Compute predicted output by passing src to the model
-        output = model(src.long().to(device))
-        B, T, C = output.shape
-        output = output.view(B*T, C)  # Reshape for loss computation
-        tgt = tgt.view(B*T).long()    # Reshape target to match output dimensions
+# training_data = training_data.to(device)
 
-        # Calculate loss
-        loss = criterion(output, tgt)
+# # Number of epochs for training
+# num_epochs = 40
 
-        # Backward pass: Compute gradient of the loss with respect to model parameters
-        loss.backward()
+# for epoch in range(num_epochs):
+#     model.train()  # Set the model to training mode
+#     total_loss = 0
 
-        # Perform a single optimization step (parameter update)
-        optimizer.step()
+#     # Iterate over the training data in batches
+#     for _ in tqdm(range(training_data.shape[1] // batch_size), desc="Batch"):
+#         # Generate a batch of data
+#         src, tgt = get_batch(training_data, batch_size, max_seq_length, device)
 
-        total_loss += loss.item()  # Accumulate the loss
+#         optimizer.zero_grad()  # Clear the gradients from the previous iteration
 
-    # Compute average loss for the epoch
-    avg_loss = total_loss / (training_data.shape[1] // batch_size)
-    print(f'Epoch: {epoch}, Average Loss: {avg_loss}')
+#         # Forward pass: Compute predicted output by passing src to the model
+#         output = model(src.long().to(device))
+#         B, T, C = output.shape
+#         output = output.view(B*T, C)  # Reshape for loss computation
+#         tgt = tgt.view(B*T).long()    # Reshape target to match output dimensions
 
-# Save model
-torch.save(model.state_dict(), 'model.pt')
+#         # Calculate loss
+#         loss = criterion(output, tgt)
+
+#         # Backward pass: Compute gradient of the loss with respect to model parameters
+#         loss.backward()
+
+#         # Perform a single optimization step (parameter update)
+#         optimizer.step()
+
+#         total_loss += loss.item()  # Accumulate the loss
+
+#     # Compute average loss for the epoch
+#     avg_loss = total_loss / (training_data.shape[1] // batch_size)
+#     print(f'Epoch: {epoch}, Average Loss: {avg_loss}')
+
+# # Save model
+# torch.save(model.state_dict(), 'model.pt')
--- src/stocksai/ai/utils.py
+++ src/stocksai/ai/utils.py
@@ -1,48 +1,48 @@
-import torch
+# import torch
 
-def get_batch(data, batch_size, block_size, device, noise_stdev=1):
-    """
-    Generate a batch of data with added noise.
+# def get_batch(data, batch_size, block_size, device, noise_stdev=1):
+#     """
+#     Generate a batch of data with added noise.
 
-    Parameters:
-    data (Tensor): The input data.
-    batch_size (int): Number of sequences in a batch.
-    block_size (int): Size of each data block.
-    device (str): The device to use ('cuda' or 'cpu').
-    noise_stdev (float): Standard deviation of the noise.
+#     Parameters:
+#     data (Tensor): The input data.
+#     batch_size (int): Number of sequences in a batch.
+#     block_size (int): Size of each data block.
+#     device (str): The device to use ('cuda' or 'cpu').
+#     noise_stdev (float): Standard deviation of the noise.
 
-    Returns:
-    Tuple[Tensor, Tensor]: Tensors x and y representing the input and target data.
-    """
-    # If data is 1-dimensional, add an extra dimension
-    if len(data.shape) == 1:
-        data = data.unsqueeze(0)
-
-
-    ix = torch.randint(0, len(data), (batch_size,))
-    iy = torch.randint(0, len(data[0]) - block_size, (batch_size,))
-    x = torch.stack([data[j][i:i+block_size] for j, i in zip(ix, iy)])
-    y = torch.stack([data[j][i+1:i+block_size+1] for j, i in zip(ix, iy)])
-
-    # Add Gaussian noise
-    noise = torch.normal(mean=0, std=noise_stdev, size=y.size())
-    y = (y.float() + noise).round().long()
-
-    return x.to(device), y.to(device)
+#     Returns:
+#     Tuple[Tensor, Tensor]: Tensors x and y representing the input and target data.
+#     """
+#     # If data is 1-dimensional, add an extra dimension
+#     if len(data.shape) == 1:
+#         data = data.unsqueeze(0)
 
 
-def append_to_tensor(original_tensor, values_to_append):
-    # Convert values_to_append to a tensor if it is not already
-    if not isinstance(values_to_append, torch.Tensor):
-        values_to_append = torch.tensor(values_to_append,
-                                        dtype=original_tensor.dtype)
+#     ix = torch.randint(0, len(data), (batch_size,))
+#     iy = torch.randint(0, len(data[0]) - block_size, (batch_size,))
+#     x = torch.stack([data[j][i:i+block_size] for j, i in zip(ix, iy)])
+#     y = torch.stack([data[j][i+1:i+block_size+1] for j, i in zip(ix, iy)])
 
-    # Create a new tensor with the size of the original tensor plus the size of values_to_append
-    new_tensor_size = original_tensor.nelement() + values_to_append.nelement()
-    new_tensor = torch.empty(new_tensor_size, dtype=original_tensor.dtype)
+#     # Add Gaussian noise
+#     noise = torch.normal(mean=0, std=noise_stdev, size=y.size())
+#     y = (y.float() + noise).round().long()
 
-    # Fill the new tensor with the original values and the new values
-    new_tensor[:original_tensor.nelement()] = original_tensor
-    new_tensor[original_tensor.nelement():] = values_to_append
+#     return x.to(device), y.to(device)
 
-    return new_tensor
+
+# def append_to_tensor(original_tensor, values_to_append):
+#     # Convert values_to_append to a tensor if it is not already
+#     if not isinstance(values_to_append, torch.Tensor):
+#         values_to_append = torch.tensor(values_to_append,
+#                                         dtype=original_tensor.dtype)
+
+#     # Create a new tensor with the size of the original tensor plus the size of values_to_append
+#     new_tensor_size = original_tensor.nelement() + values_to_append.nelement()
+#     new_tensor = torch.empty(new_tensor_size, dtype=original_tensor.dtype)
+
+#     # Fill the new tensor with the original values and the new values
+#     new_tensor[:original_tensor.nelement()] = original_tensor
+#     new_tensor[original_tensor.nelement():] = values_to_append
+
+#     return new_tensor
